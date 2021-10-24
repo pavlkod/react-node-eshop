540093affd6ef31317ec1947b72ec5e7f1d26de0
Pavel.K <pavelweb74@gmail.com>
2021-10-24 15:28:13 +0500

    Add relative router '/api/'

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const sequelize = require("./db");
 const models = require("./models/models");
 const cors = require("cors");
+const router = require("./routes");
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api", router);
 
 const start = async () => {
   try {
