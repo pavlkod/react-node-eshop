87e438926e4eaae169448077fe9d5e8e850acdae
Pavel.K <pavelweb74@gmail.com>
2021-10-28 21:15:55 +0500

    Implement getAll method in DeviceController

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -17,7 +17,24 @@ class DeviceController {
       next(ApiError.badRequest(e.message));
     }
   }
-  async getAll(req, res) {}
+  async getAll(req, res) {
+    let { brandId, typeId, limit = 10, page = 1 } = req.query;
+    let offset = page * limit - limit;
+    let devices;
+    if (!brandId && !typeId) {
+      devices = await Device.findAndCountAll({ limit, offset });
+    }
+    if (brandId && !typeId) {
+      devices = await Device.findAndCountAll({ where: { brandId }, limit, offset });
+    }
+    if (!brandId && typeId) {
+      devices = await Device.findAndCountAll({ where: { typeId }, limit, offset });
+    }
+    if (brandId && typeId) {
+      devices = await Device.findAndCountAll({ where: { typeId, brandId }, limit, offset });
+    }
+    return res.json(devices);
+  }
   async get(req, res) {}
 }
 
