5500858ef7d4cf9c7852a1c3cee5cb7a725a7536
Pavel.K <pavelweb74@gmail.com>
2021-10-29 22:11:31 +0500

    Add characters for device

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,16 +1,26 @@
 const uuid = require("uuid");
 const path = require("path");
-const { Device } = require("../models/models");
+const { Device, DeviceInfo } = require("../models/models");
 const ApiError = require("../error/ApiError");
 class DeviceController {
   async create(req, res, next) {
     try {
-      const { name, price, brandId, typeId } = req.body;
+      let { name, price, brandId, typeId, info } = req.body;
       const { img } = req.files;
       let filename = uuid.v4() + ".jpg";
       img.mv(path.resolve(__dirname, "..", "static", filename));
 
       const device = await Device.create({ name, price, brandId, typeId, img: filename });
+      if (info) {
+        info = JSON.parse(info);
+        info.forEach(i => {
+          DeviceInfo.create({
+            title: i.title,
+            description: i.description,
+            deviceId: device.id,
+          });
+        });
+      }
 
       return res.json(device);
     } catch (e) {
--- server/models/models.js
+++ server/models/models.js
@@ -64,7 +64,7 @@ Device.belongsTo(Type);
 Brand.hasMany(Device);
 Device.belongsTo(Brand);
 
-Device.hasMany(DeviceInfo);
+Device.hasMany(DeviceInfo, { as: "info" });
 DeviceInfo.belongsTo(Device);
 
 Device.hasMany(BasketDevice);
