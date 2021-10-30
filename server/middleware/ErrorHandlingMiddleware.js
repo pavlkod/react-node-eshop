c36451828346f2a7972e40e8b81384200577332a
Pavel.K <pavelweb74@gmail.com>
2021-10-30 17:41:58 +0500

    Fix error from ApiError

--- server/middleware/ErrorHandlingMiddleware.js
+++ server/middleware/ErrorHandlingMiddleware.js
@@ -1,6 +1,6 @@
 const ApiError = require("../error/ApiError");
 module.exports = function (err, req, res, next) {
-  if (err in ApiError) {
+  if (err instanceof ApiError) {
     return res.status(err.status).json({ message: err.message });
   }
   return res.status(500).json({ message: "Unhandled error" });
