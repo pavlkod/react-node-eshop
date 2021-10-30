7048e3af53fc2393bacae3f82899b7ccfdfb3a82
Pavel.K <pavelweb74@gmail.com>
2021-10-30 17:30:45 +0500

    Implements registration method in userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,32 @@
+const ApiError = require("../error/ApiError");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { User, Basket } = require("../models/models");
 class UserController {
-  async registration(req, res) {}
+  async registration(req, res, next) {
+    const { email, password, role } = req.body;
+
+    if (!email || !password) {
+      return next(ApiError.badRequest("Empty email or password"));
+    }
+    const candidate = await User.findOne({ where: { email } });
+    if (candidate) {
+      return next(ApiError.badRequest("User already exists"));
+    }
+    const hashpwd = await bcrypt.hash(password, 5);
+    console.log({ email, password: hashpwd, role });
+    const user = await User.create({ email, password: hashpwd, role });
+    const basket = await Basket.create({ userId: user.id });
+    const token = jwt.sign({ id: user.id, email, role }, process.env.SECRET_KEY_JWT, { expiresIn: "24h" });
+    return res.json(token);
+  }
   async login(req, res) {}
-  async check(req, res) {
-    res.json(req.query);
+  async check(req, res, next) {
+    const { id } = req.query;
+    if (!id) {
+      return next(ApiError.badRequest("No transfer parameter ID"));
+    }
+    res.json(id);
   }
 }
 
