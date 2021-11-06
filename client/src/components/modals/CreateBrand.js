a0bf12ac97d4574bf7697f6681143b8a484d55c3
Pavel.K <pavelweb74@gmail.com>
2021-11-06 15:43:18 +0500

    Add modals for create new elements

---
+++ client/src/components/modals/CreateBrand.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Form, Modal, Button } from "react-bootstrap";
+
+const CreateBrand = ({ show, onHide }) => {
+  return (
+    <Modal show={show} size="lg" aria-labelledby="contained-modal-title-vcenter" centered onHide={onHide}>
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">Добавить бренд</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Control placeholder="Input type..." />
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={onHide} variant="outline-primary">
+          Close
+        </Button>
+        <Button onClick={onHide} variant="outline-success">
+          Add
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default CreateBrand;
--- client/src/page/Admin.js
+++ client/src/page/Admin.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Button } from "react-bootstrap";
+import CreateBrand from "../components/modals/CreateBrand";
+import CreateDevice from "../components/modals/CreateDevice";
+import CreateType from "../components/modals/CreateType";
 
 const Admin = () => {
+  const [showTypeModal, setShowTypeModal] = useState(false);
+  const [showBrandModal, setShowBrandModal] = useState(false);
+  const [showDeviceModal, setShowDeviceModal] = useState(false);
   return (
     <Container className="d-flex flex-column gap-3 mt-3">
-      <Button variant="outline-primary">Add type</Button>
-      <Button variant="outline-primary">Add brand</Button>
-      <Button variant="outline-primary">Add device</Button>
+      <Button variant="outline-primary" onClick={() => setShowTypeModal(true)}>
+        Add type
+      </Button>
+      <Button variant="outline-primary" onClick={() => setShowBrandModal(true)}>
+        Add brand
+      </Button>
+      <Button variant="outline-primary" onClick={() => setShowDeviceModal(true)}>
+        Add device
+      </Button>
+      <CreateType show={showTypeModal} onHide={() => setShowTypeModal(false)} />
+      <CreateBrand show={showBrandModal} onHide={() => setShowBrandModal(false)} />
+      <CreateDevice show={showDeviceModal} onHide={() => setShowDeviceModal(false)} />
     </Container>
   );
 };
