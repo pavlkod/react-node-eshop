a999efbc22d1a675ce251093c1c172799a3d0084
Pavel.K <pavelweb74@gmail.com>
2021-11-06 20:40:09 +0500

    Add type, brand and props

--- client/src/components/modals/CreateDevice.js
+++ client/src/components/modals/CreateDevice.js
@@ -1,7 +1,13 @@
-import React from "react";
-import { Form, Modal, Button } from "react-bootstrap";
+import React, { useContext, useState } from "react";
+import { Form, Modal, Button, Dropdown, Row, Col } from "react-bootstrap";
+import { Context } from "../..";
 
 const CreateDevice = ({ show, onHide }) => {
+  const { device } = useContext(Context);
+  const [props, setProps] = useState([]);
+  const addInfo = () => {
+    setProps([...props, { title: "", description: "", number: Date.now() }]);
+  };
   return (
     <Modal show={show} size="lg" aria-labelledby="contained-modal-title-vcenter" centered onHide={onHide}>
       <Modal.Header closeButton>
@@ -9,7 +15,43 @@ const CreateDevice = ({ show, onHide }) => {
       </Modal.Header>
       <Modal.Body>
         <Form>
-          <Form.Control placeholder="Input type..." />
+          <Dropdown className="mt-3">
+            <Dropdown.Toggle>Choose type</Dropdown.Toggle>
+            <Dropdown.Menu>
+              {device.types.map(type => (
+                <Dropdown.Item key={type.id}>{type.name}</Dropdown.Item>
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
+          <Dropdown className="mt-3">
+            <Dropdown.Toggle>Choose brand</Dropdown.Toggle>
+            <Dropdown.Menu>
+              {device.brands.map(brand => (
+                <Dropdown.Item key={brand.id}>{brand.name}</Dropdown.Item>
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
+
+          <Form.Control placeholder="Input device..." className="mt-3" />
+          <Form.Control placeholder="Input price..." className="mt-3" type="number" />
+          <Form.Control placeholder="Input image..." className="mt-3" type="file" />
+          <hr />
+          <Button variant="outline-dark" onClick={addInfo}>
+            Add new prop
+          </Button>
+          {props.map((prop, i) => (
+            <Row key={i} className="mt-3">
+              <Col md={4}>
+                <Form.Control placeholder="Input title" />
+              </Col>
+              <Col md={4}>
+                <Form.Control placeholder="Input description" />
+              </Col>
+              <Col md={4}>
+                <Button variant={"outline-danger"}>Remove</Button>
+              </Col>
+            </Row>
+          ))}
         </Form>
       </Modal.Body>
       <Modal.Footer>
