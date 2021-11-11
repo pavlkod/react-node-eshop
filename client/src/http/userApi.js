62b96c231f8824c2f360dd9d215dfeba429f6fe6
Pavel.K <pavelweb74@gmail.com>
2021-11-11 22:19:27 +0500

    Set token in localstorage

--- client/src/http/userApi.js
+++ client/src/http/userApi.js
@@ -1,15 +1,18 @@
-import { host } from ".";
+import { host, authHost } from ".";
 import jwt_decode from "jwt-decode";
 
 export const registration = async (email, password) => {
   const { data } = await host.post("api/user/registration/", { email, password, role: "ADMIN" });
+  localStorage.setItem("token", data.token);
   return jwt_decode(data.token);
 };
 export const login = async (email, password) => {
   const { data } = await host.post("api/user/login/", { email, password });
+  localStorage.setItem("token", data.token);
   return jwt_decode(data.token);
 };
 export const check = async () => {
-  const response = await host.post("api/user/check/");
-  return response;
+  const { data } = await authHost.get("api/user/auth/");
+  localStorage.setItem("token", data.token);
+  return jwt_decode(data.token);
 };
