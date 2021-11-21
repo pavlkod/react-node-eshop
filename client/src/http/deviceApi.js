4f3d669def5e4cd4de8825865f28719e37063b43
Pavel.K <pavelweb74@gmail.com>
2021-11-21 17:08:25 +0500

    Remove unused code

--- client/src/http/deviceApi.js
+++ client/src/http/deviceApi.js
@@ -1,5 +1,4 @@
 import { host, authHost } from ".";
-import jwt_decode from "jwt-decode";
 
 export const createType = async type => {
   const { data } = await authHost.post("api/type", type);
@@ -8,9 +7,4 @@ export const createType = async type => {
 export const fetchTypes = async () => {
   const { data } = await host.post("api/type/");
   return data;
-};
-export const check = async () => {
-  const { data } = await authHost.get("api/user/auth/");
-  localStorage.setItem("token", data.token);
-  return jwt_decode(data.token);
 };
