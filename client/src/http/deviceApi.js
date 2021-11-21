678f18bae42dd4a2af20ec6a72a98533a550e701
Pavel.K <pavelweb74@gmail.com>
2021-11-21 21:00:11 +0500

    Add brand requests

--- client/src/http/deviceApi.js
+++ client/src/http/deviceApi.js
@@ -8,3 +8,12 @@ export const fetchTypes = async () => {
   const { data } = await host.get("api/type/");
   return data;
 };
+
+export const createBrand = async brand => {
+  const { data } = await authHost.post("api/brand", brand);
+  return data;
+};
+export const fetchBrands = async () => {
+  const { data } = await host.get("api/brand/");
+  return data;
+};
