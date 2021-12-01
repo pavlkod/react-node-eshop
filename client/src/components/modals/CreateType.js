8382a69945aa8e9eedb5393e12ce82900f965481
Pavel.K <pavelweb74@gmail.com>
2021-12-01 19:46:39 +0500

    Add handler for create type

--- client/src/components/modals/CreateType.js
+++ client/src/components/modals/CreateType.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Modal, Button, Form } from "react-bootstrap";
+import { createType } from "../../http/deviceApi";
 
 const CreateType = ({ show, onHide }) => {
+  const [value, setValue] = useState("");
+  const addType = () => {
+    createType({ name: value }).then(() => {
+      setValue("");
+      onHide();
+    });
+  };
   return (
     <Modal show={show} size="lg" aria-labelledby="contained-modal-title-vcenter" centered onHide={onHide}>
       <Modal.Header closeButton>
@@ -9,14 +17,14 @@ const CreateType = ({ show, onHide }) => {
       </Modal.Header>
       <Modal.Body>
         <Form>
-          <Form.Control placeholder="Input type..." />
+          <Form.Control placeholder="Input type..." value={value} onChange={e => setValue(e.target.value)} />
         </Form>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={onHide} variant="outline-primary">
           Close
         </Button>
-        <Button onClick={onHide} variant="outline-success">
+        <Button onClick={addType} variant="outline-success">
           Add
         </Button>
       </Modal.Footer>
