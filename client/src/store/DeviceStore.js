fa7fd17f44dcbd0a70c4cde8bd0b85d47662271a
Pavel.K <pavelweb74@gmail.com>
2021-12-11 18:35:57 +0500

    Set first page by filter

--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -24,9 +24,11 @@ export default class DeviceStore {
     this._devices = devices;
   }
   setSelectedType(type) {
+    this.setPage(1);
     this._selectedType = type;
   }
   setSelectedBrand(brand) {
+    this.setPage(1);
     this._selectedBrand = brand;
   }
   setPage(page) {
